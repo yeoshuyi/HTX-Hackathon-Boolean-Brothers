70e9c6e325844e97021d99c8e8f5d7cdefc39b7c
yeoshuyi <yeoshuyi123@gmail.com>
2025-10-04 11:06:59 +0800

    LAS Lib and basic test

---
+++ main.py
@@ -0,0 +1,14 @@
+import lasio
+import pandas as pd
+import matplotlib.pyplot as plt
+
+las_file_path = './test.las'
+las = lasio.read(las_file_path)
+
+print(f"Well Name: {las.well.WELL.value}")
+print(f"Field: {las.well.FLD.value}")
+
+df = las.df()
+
+plt.plot(df.index, df['GR'])
+plt.show()
