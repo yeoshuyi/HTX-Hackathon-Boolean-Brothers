071b8b38fce839dee09e1fe4ff0625d12da88e6f
yeoshuyi <yeoshuyi123@gmail.com>
2025-10-14 15:08:34 +0800

    Comment Out

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 las_file_path = './test.las'
 las = lasio.read(las_file_path)
 
-print(f"Well Name: {las.well.WELL.value}")
-print(f"Field: {las.well.FLD.value}")
+#print(f"Well Name: {las.well.WELL.value}")
+#print(f"Field: {las.well.FLD.value}")
 
 df = las.df()
 
